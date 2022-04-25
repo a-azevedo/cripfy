324da02047996bd9af7c26bf379362cc94a6725a
a-azevedo <aelmajan.azevedo@gmail.com>
2022-04-25 12:17:24 -0300

    Algumas mudanças

--- js/escript.js
+++ js/escript.js
@@ -2,20 +2,23 @@ var bt1 = document.getElementById("cript_bt");
 bt1.addEventListener("click", criptografar);
 var bt2 = document.getElementById("decript_bt");
 bt2.addEventListener("click", descriptografar);
+var bt3 = document.getElementById("bt_copy").style.value = "none";
 var txtEntrada = document.getElementById("texto_de_entrada");
-var txtSaida = document.getElementById("t_result");
+var txtSaida = document.getElementById("t_result").value;
 
-function criptografar() {
+
+function criptografar() {    
     var texto = txtEntrada.value;
-    var result = texto.toLowerCase();
-    document.getElementById("t_result").innerHTML = result.replace(/a/g, "ai").replace(/e/g, "enter").replace(/i/g, "imes").replace(/o/g, "ober").replace(/u/g, "ufat");
-    result = "";
+    var result = texto.toLowerCase();        
+    /* document.getElementById("t_result").innerHTML */txtSaida = result.replaceAll(/e/gi, "enter").replaceAll(/i/gi, "imes").replaceAll(/a/gi, "ai").replaceAll(/o/gi, "ober").replaceAll(/u/gi, "ufat");
+    document.getElementById("t_result").innerHTML = txtSaida;
+    document.getElementById("bt_copy").style.display = "block";
+          
 }
 
 function descriptografar() {
     var texto = txtEntrada.value;
     var result = texto;
-    document.getElementById("t_result").innerHTML = result.replace(/ai/g, "a").replace(/enter/g, "e").replace(/imes/g, "i").replace(/ober/g, "o").replace(/ufat/g, "u");
-    result = "";
+    document.getElementById("t_result").innerHTML = result.replace(/ai/gi, "a").replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ober/gi, "o").replace(/ufat/gi, "u");    
    
 }
